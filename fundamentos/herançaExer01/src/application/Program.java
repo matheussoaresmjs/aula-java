0bf590279969077c19b0b5d4dfa4c78524d8c7ce
Matheus Soares <matheussoaresmjs@gmail.com>
2023-11-20 20:09:04 -0300

    Herança - upcasting e downcasting

--- fundamentos/herançaExer01/src/application/Program.java
+++ fundamentos/herançaExer01/src/application/Program.java
@@ -1,7 +1,22 @@
 package application;
 
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 public class Program {
     public static void main(String[] args) throws Exception {
         
+        Account acc = new Account(1001, "Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1001, "Maria", 0.0, 500.0);
+
+        // UPCASTING
+
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+        // DOWNCASTING 
+
     }
 }
